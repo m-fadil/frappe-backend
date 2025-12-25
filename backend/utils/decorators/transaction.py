41fcf2e4e562ca72ecb9dad4ddf3d863a84d724e
fadil <wrk.fadil@gmail.com>
2025-12-25 17:12:58 +0700

    feat: Add unit tests for validator and request handlers
    - Add unit tests for validator decorator and request handling functions to ensure correct behavior and error handling.
    - Add transaction management decorator for handling database operations.
    - Refactor validation logic to consolidate error handling into a single dictionary and improve clarity of error messages.

---
+++ backend/utils/decorators/transaction.py
@@ -0,0 +1,21 @@
+import frappe
+from functools import wraps
+
+
+def with_transaction(func):
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		try:
+			frappe.db.begin()
+			result = func(*args, **kwargs)
+
+			if not frappe.flags.in_test:
+				frappe.db.commit()
+
+			return result
+		except Exception:
+			frappe.db.rollback()
+			raise
+
+	return wrapper
+
